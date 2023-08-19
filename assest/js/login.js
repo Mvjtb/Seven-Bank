fb6e8d8d88790eea781a5aece3a916403dd8a4c5
JenniiRamirez22 <141350882+JenniiRamirez22@users.noreply.github.com>
2023-08-19 14:38:27 -0400

    ya el boton de iniciar sesion redirige al mainAccount

--- assest/js/login.js
+++ assest/js/login.js
@@ -1,22 +1,24 @@
 // Obtener referencia al botón de inicio de sesión
 const loginBtn = document.getElementById("login-btn");
 
+const loginForm=document.getElementById('login-form');
+
 // Obtener referencias a los campos de entrada
-const usernameInput = document.getElementById("username");
-const cedulaInput = document.getElementById("cedula");
+const nameUserInput = document.getElementById("nameUser");
+const identInput = document.getElementById("ident");
 const passwordInput = document.getElementById("password");
 
 // Agregar evento al botón de inicio de sesión para mostrar un mensaje si los campos están vacíos
 loginBtn.addEventListener("click", () => {
-    if (usernameInput.value === "" || cedulaInput.value === "" || passwordInput.value === "") {
+    if (nameUserInput.value === "" || identInput.value === "" || passwordInput.value === "") {
         alert("Por favor, complete los campos para iniciar sesión.");
-    } else if (usernameInput.value === "admin" && cedulaInput.value === "admin" && passwordInput.value === "admin") {
-        window.location.href = "mainaccount.html";
+    } else if (nameUserInput.value === "admin" && identInput.value === "admin" && passwordInput.value === "admin") {
+        loginForm.reset();
+        window.location.href = "mainAccount.html";
         alert("¡Inicio de sesión exitoso!");
     }else{
         alert("Este usuario no se encuentra registrado");
         
     }
 });
-
 
